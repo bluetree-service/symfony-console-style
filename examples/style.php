da93797c62085e5038545c95481c2c0212949366
chajr <chajr@bluetree.pl>
2018-07-23 17:05:22 +0200

    Prepared some example files

---
+++ examples/style.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace SymfonyStyle\Examples;
+
+require_once '../vendor/autoload.php';
+
+use BlueConsole\Style;
+
+$input = '';
+$output = '';
+$formatter = '';
+$style = new Style($input, $output, $formatter);
