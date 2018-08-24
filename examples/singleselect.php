0c5cb6acbb5e0eeeb54d375a30f5d562ebff808c
chajr <chajr@bluetree.pl>
2018-08-24 16:32:22 +0200

    Single select example

---
+++ examples/singleselect.php
@@ -0,0 +1,31 @@
+#!/usr/bin/env php
+<?php
+
+namespace SymfonyStyle\Examples;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use BlueConsole\MultiSelect;
+use Symfony\Component\Console\{
+    Input\ArgvInput,
+    Output\ConsoleOutput,
+    Helper\FormatterHelper,
+    Style\SymfonyStyle
+};
+
+$input = new ArgvInput;
+$output = new ConsoleOutput;
+
+$multiSelect = new MultiSelect(new SymfonyStyle($input, $output));
+
+$list = ['first', 'second', 'last'];
+$output->writeln('Data list:');
+
+dump($list);
+$output->writeln('');
+
+$val = $multiSelect->renderSingleSelect($list);
+
+$output->writeln('');
+$output->writeln('Method output:');
+dump($val);
