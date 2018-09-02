a2d6f24ceb79b21cec233f1470ea32ba339a8218
chajr <chajr@bluetree.pl>
2018-09-02 12:55:45 +0200

    Animation update, added some empty methods

--- src/Animation.php
+++ src/Animation.php
@@ -4,7 +4,12 @@ namespace BlueConsole;
 
 class Animation
 {
-    public function __construct($steps = 6, $cursor = '*')
+    /**
+     * @var \DateInterval
+     */
+    protected $interval;
+
+    public function __construct(int $steps = 6, string $cursor = '*')
     {
         $counter = 0;
         $forward = true;
@@ -46,5 +51,70 @@ class Animation
 
             sleep(1);
         }
+    }
+
+    public function setWaitTime(\DateInterval $dateInterval)
+    {
+        
+    }
+
+    public function setMessage(string $message)
+    {
+        
+    }
+
+    public function run($process)
+    {
+        //display message with animation
+        //execute external process
+        //wait and display animation
+        //process ended -> display message and go fodder
+    }
+
+    protected function displayAnimation()
+    {
+        $counter = 0;
+        $forward = true;
+        $before = 0;
+        $after = $steps - \strlen($cursor);
+
+        while (true) {
+            echo '[';
+
+            for ($i = 1; $i <= $before; $i++) {
+                echo ' ';
+            }
+
+            echo $cursor;
+
+            for ($i = 0; $i <= $after; $i++) {
+                echo ' ';
+            }
+
+            echo ']';
+            echo "\r";
+
+            $counter++;
+
+            if ($forward) {
+                $before++;
+                $after--;
+            } else {
+                $before--;
+                $after++;
+            }
+
+            if ($counter === ($steps - (\strlen($cursor) -1))) {
+                $counter = 0;
+                $forward = !$forward;
+            }
+
+            sleep(1);
+        }
+    }
+
+    protected function executeProcess()
+    {
+        
     }
 }
